95307c01e979d763f029a6820b32a27b0cc89d59
Juancollareda <collaredajuan@uca.edu.ar>
2025-07-21 21:58:18 -0300

    button counter

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
+const { count } = require('console');
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+counter = 0;
 // Serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
 // API endpoint for button click
 app.get('/clicked', (req, res) => {
   console.log("Button was clicked!");
+  counter += 1;
+  console.log(`Button clicked ${counter} times`);
   res.send("Thanks for clicking!");
 });
 
