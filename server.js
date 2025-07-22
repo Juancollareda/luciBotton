ddf2d05cc3319c11ae08b1362227dc3da4156227
Juancollareda <collaredajuan@uca.edu.ar>
2025-07-21 22:17:43 -0300

    change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // API endpoint for button click
