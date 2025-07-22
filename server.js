52f5960cada82e0dedaafcdfa1a9f70edcc8d171
Juancollareda <collaredajuan@uca.edu.ar>
2025-07-21 23:39:02 -0300

    here we go

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-// server.js
-const { count } = require('console');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-counter = 0;
-// Serve static files from public directory
+
+const COUNT_FILE = path.join(__dirname, 'count.txt');
+let counter = 0;
+
+
+if (fs.existsSync(COUNT_FILE)) {
+  const savedCount = fs.readFileSync(COUNT_FILE, 'utf8');
+  counter = parseInt(savedCount, 10) || 0;
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// API endpoint for button click
 app.get('/clicked', (req, res) => {
   console.log("Button was clicked!");
   counter += 1;
+
+  fs.writeFileSync(COUNT_FILE, counter.toString(), 'utf8');
   console.log(`Button clicked ${counter} times`);
   res.send(`Thanks for clicking! ${counter}`);
+});
+
+
+app.get('/count', (req, res) => {
+  res.send(`Button has been clicked ${counter} times`);
 });
 
 app.listen(PORT, () => {
